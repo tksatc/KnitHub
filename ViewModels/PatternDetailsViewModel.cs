1ccd8002c31d51f6aad1d1ee237bfd77731bd18a
tksatc <tksatc@gmail.com>
2021-12-08 12:42:23 -0800

    add pattern vm

---
+++ ViewModels/PatternDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KnitHub.Models;
+
+namespace KnitHub.ViewModels
+{
+    public class PatternDetailsViewModel
+    {
+        public Pattern Pattern { get; set; }
+        public PatternDetail PatternDetail { get; set; }
+    }
+}
